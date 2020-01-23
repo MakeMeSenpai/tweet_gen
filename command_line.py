6cebe8515f49beca0e029ba9f63f0d02992b90d1
Anthony <sean.a.protho1@gmail.com>
2020-01-23 10:44:09 -0800

    added quotes

--- command_line.py
+++
@@ -1,5 +0,0 @@
-import sys as sys
-
-if __name__ == "__main__":
-    args = sys.argv
-    print(args)
--- test.py
+++
@@ -1,6 +0,0 @@
-from random import random
-
-lines = [1, 2, 3, 4, 5]
-
-random_index = randint(0, len(lines) - 1)
-print(lines[random_index])
